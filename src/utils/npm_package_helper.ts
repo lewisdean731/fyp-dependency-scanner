5a3fdf4b612a1fb1a0d110a10141e805b0a2f015
Lewis Dean <lewisdean731@gmail.com>
2021-05-09 01:48:58 +0100

    feat: create helper functions for working with NPM files
    [Ticket: FYP-18]

---
+++ src/utils/npm_package_helper.ts
@@ -0,0 +1,38 @@
+import Axios from "axios";
+
+const importFile = async (
+  url: string,
+  BITBUCKET_USERNAME: string,
+  BITBUCKET_PASSWORD: string
+): Promise<any> => {
+  console.log("importing file: " + url);
+  await Axios.get(url, {
+    auth: {
+      username: BITBUCKET_USERNAME,
+      password: BITBUCKET_PASSWORD
+    }
+  }).then((response) => {
+    return response.data;
+  }).catch((error) => {
+    throw new Error(`Error: ${error}`);
+  });
+};
+
+function getDependencies(packageJson: PackageJson) {
+  let dependencies: string[];
+  dependencies = {
+    ...packageJson.dependencies,
+    ...packageJson.devDependencies
+  };
+  return dependencies;
+}
+
+function getLockDependencies(packageLockJson: PackageLockJson) {
+  return packageLockJson.dependencies;
+}
+
+export {
+  importFile,
+  getDependencies,
+  getLockDependencies
+};
