22d959c46415024613f504208e9fdd6508e0e59c
Lewis Dean <lewisdean731@gmail.com>
2021-05-09 16:46:52 +0100

    feat(test): cover all other function in npm_dependency_helper
    [Ticket: FYP-18]

--- src/utils/npm/npm_dependency_helper.test.ts
+++ src/utils/npm/npm_dependency_helper.test.ts
@@ -7,8 +7,9 @@ const mockedAxios = axios as jest.Mocked<typeof axios>;
 const depMetaData = {
   data: {
     name: "fakeDependency",
-    stuff: "abcd",
-    otherStuff: 999,
+    "dist-tags": {
+      latest: "1.1.0"
+    },
     versions: {
       "1.0.0": {
         name: "fakeDependency"
@@ -22,6 +23,21 @@ const depMetaData = {
       "created": "2012-02-01T04:32:55.209Z",
       "1.0.0": "2012-02-01T04:32:57.016Z",
       "1.1.0": "2012-02-03T18:04:34.851Z",
+    }
+  }
+}
+
+const packageLock = {
+  name: "package-lock",
+  version: "1.0.0",
+  lockfileVersion: 1,
+  requires: true,
+  dependencies: {
+    dependency1: {
+      version: "0.1.2"
+    },
+    dependency2: {
+      version: "2.3.4"
     }
   }
 }
@@ -53,3 +69,41 @@ describe("getDependencyMetadata", () => {
     );
   })
 });
+
+describe("getDependencyReleases", () => {
+  test("should return releases from dependency metadata", () => {
+    mockedAxios.get.mockResolvedValue(depMetaData);
+  
+    return npmDependencyHelper
+      .getDependencyReleases("fakeDependency")
+      .then(data => expect(data).toEqual(depMetaData.data.time));
+  });
+});
+
+describe("getLatestDependencyRelease", () => {
+  test("should return latest dependency release", () => {
+    mockedAxios.get.mockResolvedValue(depMetaData);
+  
+    return npmDependencyHelper
+      .getLatestDependencyRelease("fakeDependency")
+      .then(data => expect(data).toEqual("1.1.0"));
+  });
+});
+
+describe("findDependency", () => {
+  test("should find dependency version in package-lock.json", () => {
+    const version = npmDependencyHelper
+      .findDependency(packageLock, "dependency1")
+
+    expect(version).toEqual({"version": "0.1.2"})
+  });
+
+  test("returns null when dependency is not found", () => {
+    const version = npmDependencyHelper
+      .findDependency(packageLock, "nonExistentDependency")
+
+    expect(version).toEqual({"version": "null"})
+  });
+});
+
+
