8c85f162cb1073ba5beba0bf270bea8e52219a1e
Lewis Dean <lewisdean731@gmail.com>
2021-05-09 21:21:14 +0100

    fix: correct name of project

--- src/projects.ts
+++ src/projects.ts
@@ -1,7 +1,7 @@
 export const projects: ProjectList = {
   npmProjects: [
     {
-      projectName: "dependency-scanner",
+      projectName: "API",
       packageJsonUrl:
         "https://api.bitbucket.org/2.0/repositories/s5008913/api/src/master/package.json",
       packageLockDotJsonUrl:
