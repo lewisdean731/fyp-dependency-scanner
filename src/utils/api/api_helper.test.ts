2b87f977a0172147e0356a07591cd5ba90f0a30d
Lewis Dean <lewisdean731@gmail.com>
2021-05-10 17:42:08 +0100

    feat(test): add tests for get request function and fetching projects
    [Ticket: FYP-20]

--- src/utils/api/api_helper.test.ts
+++ src/utils/api/api_helper.test.ts
@@ -4,46 +4,66 @@ import axios from "axios"
 jest.mock("axios");
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
-describe("authorise", () => {
-  test("example", () => {
-    expect(true).toEqual(true)
+process.env.PROJECTS_ENDPOINT = "/api/fake/projects"
+
+describe("asyncGetRequest", () => {
+  test("returns a 200 when authorised", async () => {
+    mockedAxios.get.mockImplementation(() => Promise.resolve({
+      status: 200
+    }));
+
+    await apiHelper
+      .asyncGetRequest("fakeUrl")
+      .then(response => expect(response.status).toEqual(200));
+  });
+
+  test("returns a 401 when unauthorised", async () => {
+    mockedAxios.get.mockImplementation(() => Promise.resolve({
+      status: 401
+    }));
+
+    await apiHelper
+      .asyncGetRequest("fakeUrl")
+      .then(response => expect(response.status).toEqual(401));
   });
 });
 
 describe("getProject", () => {
-  test("should return project details when given the correct ID", () => {
+  test("should return project details when given the correct ID", async () => {
     mockedAxios.get.mockImplementation(() => Promise.resolve({
-      response: {
         status: 200, 
         data: {
-            projectName: "Fake Project",
-            team: "fakeTeamId",
-            projectType: {
-              npm: {
-                packageJsonUrl: "fakeUrl",
-                packageLockUrl: "fakeUrl"
-              }
+          projectName: "Fake Project",
+          team: "fakeTeamId",
+          projectType: {
+            npm: {
+              packageJsonUrl: "fakeUrl",
+              packageLockUrl: "fakeUrl"
             }
+          }
         } 
-      } 
     }));
 
-    const data = apiHelper
+    await apiHelper
       .getProject("fakeProjectId")
-      .then(response => expect(response.status).toEqual(200));
-
-    expect(data).toEqual(
-      {
+      .then((data) => expect(data).toEqual({
         projectName: "Fake Project",
         packageJsonUrl: "fakeUrl",
         packageLockUrl: "fakeUrl"
-      }
-    )
-
+      }));
   });
 
-  test("should return an error when project not found", () => {
-    expect(true).toEqual(true)
+  test("should return an error when project not found", async () => {
+    mockedAxios.get.mockImplementation(() => Promise.resolve({
+      status: 404, 
+      data: { error: "No such document" }
+    }));
+
+    await apiHelper
+      .getProject("fakeProjectId")
+      .then((data) => expect(data).toEqual({
+        error: "No such document"
+      }));
   });
 });
 
