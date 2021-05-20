49542faa0aa30137b9328b4b2a7fa746f0a09006
Lewis Dean <lewisdean731@gmail.com>
2021-05-20 19:18:34 +0100

    feat: add tests for notification creator function
    [Ticket: FYP-36]

--- src/utils/dependency_helper.test.ts
+++ src/utils/dependency_helper.test.ts
@@ -1,6 +1,24 @@
 import * as dependencyHelper from "./dependency_helper";
+import axios from "axios";
+
+import * as apiHelper from "./api/api_helper";
+
+const apiSpy = jest.spyOn(apiHelper, "createNotification").mockImplementation();
 
 const consoleSpy = jest.spyOn(console, "log").mockImplementation();
+
+jest.mock("axios");
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+interface ScannedDependency {
+  name: string;
+  version: string;
+  release_date: Date | number;
+  latest_version: string;
+  latest_release_date: Date | number;
+  next_version: string;
+  next_release_date: Date | number;
+};
 
 const scannedDependencies: ScannedDependency[] = [
   {
@@ -29,6 +47,24 @@ const scannedDependencies: ScannedDependency[] = [
     release_date: new Date("2021-04-05T20:58:03.835Z"),
     latest_release_date: new Date("2021-04-05T20:58:03.835Z"),
     next_release_date: new Date("2021-04-05T20:58:03.835Z"),
+  },
+  {
+    name: "dependency4",
+    version: "2.3.4",
+    latest_version: "2.3.5",
+    next_version: "2.3.5",
+    release_date: new Date(0),
+    latest_release_date: (new Date().getTime() - 9.504e+8), // 11 days ago
+    next_release_date: (new Date().getTime() - 9.504e+8),
+  },
+  {
+    name: "dependency5",
+    version: "2.3.4",
+    latest_version: "2.3.5",
+    next_version: "2.3.5",
+    release_date: new Date(0),
+    latest_release_date: (new Date().getTime() - 1.382e+9), // 16 days ago
+    next_release_date: (new Date().getTime() - 1.382e+9),
   },
 ];
 
@@ -119,5 +155,82 @@ describe("getDependencyReleaseDate", () => {
     expect(
       dependencyHelper.getDependencyReleaseDate("9.9.9", dependencyReleases)
     ).toEqual({ date: new Date(0) });
+  });
+});
+
+describe("createNotifications", () => {
+  
+  beforeEach(() => {
+    apiSpy.mockClear();
+  });
+
+  test("should create a notification when dependency > yellow period ", async () => {
+    mockedAxios.put.mockImplementation(() =>
+      Promise.resolve({ status: 200 })
+    );
+    await dependencyHelper.createNotifications(
+      "project Id", 
+      "project Name", 
+      [scannedDependencies[3]],
+      8.64e+8, 
+      1.296e+9, 
+    );
+    expect(apiSpy).toHaveBeenCalledTimes(1)
+    expect(apiSpy).toHaveBeenCalledWith( {"message": "'dependency4' is more than 10 days out of date", "projectId": "project Id", "projectName": "project Name", "severity": "yellow"} );
+  });
+
+  test("should create a notification when dependency > red period ", async () => {
+    mockedAxios.put.mockImplementation(() =>
+      Promise.resolve({ status: 200 })
+    );
+    await dependencyHelper.createNotifications(
+      "project Id", 
+      "project Name", 
+      [scannedDependencies[4]],
+      8.64e+8, 
+      1.296e+9, 
+    );
+    expect(apiSpy).toHaveBeenCalledTimes(1)
+    expect(apiSpy).toHaveBeenCalledWith( {"message": "'dependency5' is more than 15 days out of date", "projectId": "project Id", "projectName": "project Name", "severity": "red"} );
+  });
+
+  test("should NOT create a notification when dep. up to date ", async () => {
+    mockedAxios.put.mockImplementation(() =>
+      Promise.resolve({ status: 200 })
+    );
+    await dependencyHelper.createNotifications(
+      "project Id", 
+      "project Name", 
+      [scannedDependencies[2]],
+      8.64e+8, 
+      1.296e+9, 
+    );
+    expect(apiSpy).toHaveBeenCalledTimes(0)
+  });
+
+  test("should throw error if cannot create yellow notification ", async () => {
+    mockedAxios.put.mockImplementation(() =>
+      Promise.reject({ status: 500 })
+    );
+    await dependencyHelper.createNotifications(
+      "project Id", 
+      "project Name", 
+      [scannedDependencies[3]],
+      8.64e+8, 
+      1.296e+9, 
+    ).catch((error: any) => expect(error).toBe(""))
+  });
+
+  test("should throw error if cannot create red notification ", async () => {
+    mockedAxios.put.mockImplementation(() =>
+    Promise.reject({ status: 500 })
+    );
+    await dependencyHelper.createNotifications(
+      "project Id", 
+      "project Name", 
+      [scannedDependencies[3]],
+      8.64e+8, 
+      1.296e+9, 
+    ).catch((error: any) => expect(error).toBe(""))
   });
 });
